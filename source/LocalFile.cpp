9c558baaf0e4db243c33e7bcd499cef8536342dd
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2021-04-17 16:17:02 +0200

    Update copyright years to "2019-2021"

--- source/LocalFile.cpp
+++ source/LocalFile.cpp
@@ -1,6 +1,6 @@
 /*
     Uxplore
-    Copyright (C) 2019-2020, Kuruyia
+    Copyright (C) 2019-2021, Kuruyia
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
